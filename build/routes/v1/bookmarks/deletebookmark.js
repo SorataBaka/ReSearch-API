ae67033d0a8276ef02b4329688c75f1fda0d3122
Christian Harjuno <harjuno.tian@gmail.com>
2022-02-26 23:24:02 +0700

    rebuilt

--- build/routes/v1/bookmarks/deletebookmark.js
+++ build/routes/v1/bookmarks/deletebookmark.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const deleteBookmark = async (req, res) => {
+    return res.status(200).json({
+        message: "Bookmark deleted successfully",
+        status: 200,
+        data: {}
+    });
 };
 exports.default = deleteBookmark;
