88f35b5d24ac2d3202d5e19a9e1bfc46a8c85583
Christian Harjuno <harjuno.tian@gmail.com>
2022-02-27 19:40:02 +0700

    Implemented cors

--- build/index.js
+++ build/index.js
@@ -8,6 +8,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const compression_1 = __importDefault(require("compression"));
+const cors_1 = __importDefault(require("cors"));
 const versionrouter_1 = __importDefault(require("./routes/versionrouter"));
 const mongoconnect_1 = __importDefault(require("./utils/mongoconnect"));
 dotenv_1.default.config();
@@ -16,13 +17,14 @@ app.use(express_1.default.json());
 app.use((0, compression_1.default)());
 app.use(body_parser_1.default.json());
 app.use((0, morgan_1.default)("dev"));
+app.use((0, cors_1.default)());
 app.use("/", versionrouter_1.default);
 app.get("/", (req, res) => {
     1;
     return res.status(200).json({
         message: "ReSearch Engine API",
         status: 200,
-        data: {}
+        data: {},
     });
 });
 const PORT = process.env.PORT || 3000;
--- index.ts
+++ index.ts
@@ -1,35 +1,37 @@
-import dotenv from 'dotenv';
-import express from "express"
-import { Request, Response, NextFunction } from "express"
-import bodyParser from "body-parser"
-import morgan from "morgan"
-import compression from "compression"
+import dotenv from "dotenv";
+import express from "express";
+import { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import compression from "compression";
+import cors from "cors";
+import versionrouter from "./routes/versionrouter";
+import connectMongoose from "./utils/mongoconnect";
 
-import versionrouter from "./routes/versionrouter"
-import connectMongoose from "./utils/mongoconnect"
+dotenv.config();
+const app = express();
 
-dotenv.config()
-const app = express()
+app.use(express.json());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cors());
 
-app.use(express.json())
-app.use(compression())
-app.use(bodyParser.json())
-app.use(morgan("dev"))
+app.use("/", versionrouter);
 
-app.use("/", versionrouter)
+app.get("/", (req: Request, res: Response) => {
+	1;
+	return res.status(200).json({
+		message: "ReSearch Engine API",
+		status: 200,
+		data: {},
+	});
+});
 
-app.get("/", (req:Request, res:Response) => {1
-  return res.status(200).json({
-    message: "ReSearch Engine API",
-    status: 200,
-    data: {}
-  })
-})
-
-const PORT = process.env.PORT || 3000
-const URI = process.env.MONGO_URI || undefined
-app.listen(PORT, async() => {
-  console.log(`Server is running on port ${PORT}`)
-  await connectMongoose(URI as string)
-  console.log("Startup Complete")
-})
+const PORT = process.env.PORT || 3000;
+const URI = process.env.MONGO_URI || undefined;
+app.listen(PORT, async () => {
+	console.log(`Server is running on port ${PORT}`);
+	await connectMongoose(URI as string);
+	console.log("Startup Complete");
+});
